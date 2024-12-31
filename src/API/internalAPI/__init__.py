e48f0d91b8468e9bee84341e2981b05a9964efe8
Titanium2099 <yasgh@protonmail.com>
2024-12-31 18:51:57 +0200

    loading projects now works

--- src/API/internalAPI/__init__.py
+++ src/API/internalAPI/__init__.py
@@ -4,5 +4,7 @@ from .projectsMETA import projectsMETA_route
 
 def register_routes(app):
     app.add_url_rule("/assets", view_func=assets_route)
-    app.add_url_rule('/projectsMETA', methods=['PUT', 'GET', 'POST', 'OPTIONS'], view_func=projectsMETA_route)
+    app.add_url_rule("/assets/<path:subpath>", view_func=assets_route)
+    app.add_url_rule('/projectsMETA', methods=['PUT', 'GET', 'POST', 'OPTIONS'], view_func=projectsMETA_route)    
     app.add_url_rule("/projects", view_func=projects_route)
+    app.add_url_rule("/projects/<path:subpath>", view_func=projects_route)
