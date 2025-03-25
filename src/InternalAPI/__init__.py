8a5527b5ddf5745a5802630c5f46373721958207
Titanium2099 <yasgh@protonmail.com>
2025-03-24 18:18:30 -0700

    load projects for a specific user

--- src/InternalAPI/__init__.py
+++ src/InternalAPI/__init__.py
@@ -2,9 +2,11 @@ from .updateUserAuthToken import UUAT_routes
 from .updateProjectStatus import UPS_routes
 from .updateProjectTitle import UPT_routes
 from .deleteProject import deleteProject_routes
+from .loadProjects import LP_routes
 
 def internal_register_routes(app):
     app.add_url_rule("/internal/updateUserAuthToken", methods=['POST', 'OPTIONS'], view_func=UUAT_routes)
     app.add_url_rule("/internal/updateProjectStatus", methods=['POST', 'OPTIONS'], view_func=UPS_routes)
     app.add_url_rule("/internal/updateProjectTitle", methods=['POST', 'OPTIONS'], view_func=UPT_routes)
     app.add_url_rule("/internal/deleteProject", methods=['POST', 'OPTIONS'], view_func=deleteProject_routes)
+    app.add_url_rule("/internal/loadProjects", methods=['POST', 'OPTIONS'], view_func=LP_routes)
---
+++ src/InternalAPI/loadProjects.py
@@ -0,0 +1,34 @@
+from flask import request, jsonify
+import os
+
+from API.services.helpers import get_db_connection
+
+
+def LP_routes():
+    # Handle preflight request
+    if request.method == 'OPTIONS':
+        return ''
+    
+    # Validate the token
+    token = request.json.get('token')
+    if token != os.getenv("InternalAPIKey"):
+        return jsonify({"error": "Invalid token"}), 401
+    
+    # Extract data from request
+    username = request.json.get('username')
+
+    # Step 1: Establish DB connection
+    db_connection = get_db_connection("projects")
+    cursor = db_connection.cursor(dictionary=True)
+
+    # Step 2: Get user's projects sorted by timestamp
+    query = """
+        SELECT projectID
+        FROM projects
+        WHERE Owner = %s
+        ORDER BY EditTS DESC
+    """
+    cursor.execute(query, (username,))
+    projects = cursor.fetchall()
+    project_ids = [project['projectID'] for project in projects]
+    return jsonify({"status":"true","projectIDs": project_ids})
